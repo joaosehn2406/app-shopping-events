3f32c7b66e6bc8b9578eede8232159c99e89a0ba
joaosehn2406 <jpssantosos5874@gmail.com>
2025-06-13 10:17:26 -0300

    feat: created a reusable app bar

--- app/src/main/java/com/example/shopping_events_app/customcomp/ShoppingAppBar.kt
+++ app/src/main/java/com/example/shopping_events_app/customcomp/ShoppingAppBar.kt
@@ -1,2 +1,36 @@
 package com.example.shopping_events_app.customcomp
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ShoppingAppBar(
+    title: String,
+    canNavigateBack: Boolean,
+    modifier: Modifier = Modifier,
+    navigateUp: () -> Unit = {}
+) {
+     TopAppBar(
+        title = { Text(title) },
+        modifier = modifier,
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(
+                    onClick = {}
+                ) {
+                    Icon(Icons.AutoMirrored.Filled.ArrowBack, null)
+                }
+            }
+        }
+    )
+}
