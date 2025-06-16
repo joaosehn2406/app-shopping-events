230f9d8dc1ae8570f644e8772d29a9a6341b3a95
joaosehn2406 <jpssantosos5874@gmail.com>
2025-06-16 08:56:47 -0300

    feat: shopping event data class created

---
+++ app/src/main/java/com/example/shopping_events_app/data/entities/ShoppingEvent.kt
@@ -0,0 +1,11 @@
+package com.example.shopping_events_app.data.entities
+
+data class ShoppingEvent(
+    val id: Long = 0,
+    val name: String,
+    val initialBudget: Double = 0.0,
+    val totalCost: Double,
+    val eventDate: String,
+    val completed: Boolean = false
+)
+
