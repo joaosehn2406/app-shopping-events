d3f601c1a55e3d5a1f3e8a0db93f48bbd0ccf1d2
joaosehn2406 <jpssantosos5874@gmail.com>
2025-06-16 17:05:47 -0300

    feat: created the inputfield texts

--- app/src/main/java/com/example/shopping_events_app/ui/addevent/AddEventPage.kt
+++ app/src/main/java/com/example/shopping_events_app/ui/addevent/AddEventPage.kt
@@ -1,32 +1,102 @@
 package com.example.shopping_events_app.ui.addevent
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.shopping_events_app.customcomp.ShoppingAppBar
 
 @Composable
 fun AddEventPage(
     navigateBack: () -> Unit,
     navigateUp: () -> Unit,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    viewModel: AddEventViewModel = hiltViewModel()
 ) {
     Scaffold(
         topBar = {
             ShoppingAppBar(
                 title = "Add Event",
+                modifier = modifier
+                    .size(16.dp),
                 canNavigateBack = true,
                 navigateUp = navigateUp,
                 navigateBack = navigateBack
             )
         }
-    ) {
-            innerPadding ->
-        Text(
-            text = "Add Event",
+    ) {  innerPadding ->
+        EventForm(
+            uiState = viewModel.addEventUiState,
+            onEventValueChange = viewModel::updateUiState,
+            onSaveClick = {},
             modifier = modifier.padding(innerPadding)
         )
+    }
+}
+
+
+@Composable
+fun EventForm(
+    uiState: AddEventUiState,
+    onEventValueChange: (AddEventDetails) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .padding(8.dp)
+            .verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {  }
+}
+
+@Composable
+fun TextInputField(
+    addEventDetails: AddEventDetails,
+    onEventValueChange: (AddEventDetails) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier) {
+        OutlinedTextField(
+            value = addEventDetails.name,
+            keyboardOptions = KeyboardOptions.Default.copy(
+                imeAction = ImeAction.Next
+            ),
+            onValueChange = {
+                onEventValueChange(addEventDetails.copy(name = it))
+            },
+            label = {
+                Text(text = "Event name")
+            },
+            modifier = modifier.fillMaxWidth().padding(8.dp)
+        )
+        OutlinedTextField(
+            value = addEventDetails.initialBudget,
+            keyboardOptions = KeyboardOptions.Default.copy(
+                imeAction = ImeAction.Done
+            ),
+            onValueChange = {
+                onEventValueChange(addEventDetails.copy(name = it))
+            },
+            label = {
+                Text(text = "Initial Budget (Optional)")
+            },
+            modifier = modifier.fillMaxWidth().padding(8.dp)
+        )
+
     }
 }
--- app/src/main/java/com/example/shopping_events_app/ui/addevent/AddEventUiState.kt
+++ app/src/main/java/com/example/shopping_events_app/ui/addevent/AddEventUiState.kt
@@ -1,11 +1,9 @@
 package com.example.shopping_events_app.ui.addevent
 
-import com.example.shopping_events_app.ui.eventdetails.AddEventDetailsPage
-
 data class AddEventDetails(
     val id: Long = 0,
     val name: String = "",
-    val initialBudget: Double = 0.0,
+    val initialBudget: String = "0.0",
     val totalCost: Double = 0.0,
     val eventDate: String = "",
     val completed: Boolean = false
