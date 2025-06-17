520257b7118fb823403f0df8d1cd01fe4e5d01c9
joaosehn2406 <jpssantosos5874@gmail.com>
2025-06-17 16:42:55 -0300

    feat: created ShoppingDB to establish DB connection

---
+++ app/src/main/java/com/example/shopping_events_app/data/ShoppingDB.kt
@@ -0,0 +1,21 @@
+package com.example.shopping_events_app.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.shopping_events_app.data.dao.ShoppingEventDao
+import com.example.shopping_events_app.data.dao.ShoppingItemDao
+import com.example.shopping_events_app.data.entities.ShoppingEvent
+import com.example.shopping_events_app.data.entities.ShoppingItem
+
+@Database(entities = [ShoppingEvent::class, ShoppingItem::class], version = 1)
+abstract class ShoppingDB : RoomDatabase() {
+    abstract fun shoppingEventDao(): ShoppingEventDao
+    abstract fun shoppingItemDao(): ShoppingItemDao
+
+    companion object {
+        const val DATABASE_NAME = "shopping_database"
+
+    }
+}
