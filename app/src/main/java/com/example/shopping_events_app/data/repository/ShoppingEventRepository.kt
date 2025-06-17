1c5203fec33632782fe8cdfd545e36cc00154368
joaosehn2406 <jpssantosos5874@gmail.com>
2025-06-17 17:16:42 -0300

    saving project

--- app/src/main/java/com/example/shopping_events_app/data/repository/ShoppingEventRepository.kt
+++ app/src/main/java/com/example/shopping_events_app/data/repository/ShoppingEventRepository.kt
@@ -1,11 +1,34 @@
 package com.example.shopping_events_app.data.repository
 
+import com.example.shopping_events_app.data.dao.ShoppingEventDao
 import com.example.shopping_events_app.data.entities.ShoppingEvent
 import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
 
 interface ShoppingEventRepository {
     suspend fun insert(shoppingEvent: ShoppingEvent)
     suspend fun update(shoppingEvent: ShoppingEvent)
     suspend fun delete(shoppingEvent: ShoppingEvent)
     fun getEvents(): Flow<List<ShoppingEvent>>
+}
+
+class ShoppingEventRepositoryImpl @Inject constructor(
+    private val shoppingEventDao: ShoppingEventDao
+) : ShoppingEventRepository {
+    override suspend fun insert(shoppingEvent: ShoppingEvent) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun update(shoppingEvent: ShoppingEvent) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun delete(shoppingEvent: ShoppingEvent) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getEvents(): Flow<List<ShoppingEvent>> {
+        TODO("Not yet implemented")
+    }
+
 }
