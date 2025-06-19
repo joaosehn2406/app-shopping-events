7c673f11a24f88b46d23e0e2c0d1b749d4a49e63
João Pedro <joao.pedro@joinin.com.br>
2025-06-19 09:38:20 -0300

    feat: collect and update events state in ViewModel"

--- app/src/main/java/com/example/shopping_events_app/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/shopping_events_app/ui/home/HomeViewModel.kt
@@ -1,10 +1,14 @@
 package com.example.shopping_events_app.ui.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.shopping_events_app.data.repository.ShoppingEventRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -14,4 +18,14 @@ class HomeViewModel @Inject constructor(
     private val _homeUiState = MutableStateFlow(HomeUiState())
     val homeUiState = _homeUiState.asStateFlow()
 
+    init {
+        viewModelScope.launch {
+            shoppingEventRepository.getEvents()
+                .collect { events ->
+                    _homeUiState.update {
+                        it.copy(events = events)
+                    }
+                }
+        }
+    }
 }
