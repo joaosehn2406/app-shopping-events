384b213350d3bee95118e5a043c0109525df4a65
João Pedro <joao.pedro@joinin.com.br>
2025-06-19 10:01:50 -0300

    feat: created emptyList method and validations to manipulate data if the list of events is empty

--- app/src/main/java/com/example/shopping_events_app/ui/home/HomePage.kt
+++ app/src/main/java/com/example/shopping_events_app/ui/home/HomePage.kt
@@ -1,21 +1,32 @@
 package com.example.shopping_events_app.ui.home
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FabPosition
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.shopping_events_app.customcomp.ShoppingAppBar
-
 @Composable
 fun HomePage(
     navigateToAddEvent: () -> Unit,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    viewModel: HomeViewModel = hiltViewModel()
 ) {
+
+    val uiState by viewModel.homeUiState.collectAsState()
+
     Scaffold(
         topBar = {
             ShoppingAppBar(
@@ -29,12 +40,33 @@ fun HomePage(
             ) {
                 Icon(Icons.Filled.Add, null)
             }
+        },
+        floatingActionButtonPosition = FabPosition.Center
+    ) { innerPadding ->
+        if (uiState.events.isEmpty()) {
+            EmptyList(
+                message = "No events found\nAdd an event to get started",
+                modifier = modifier.padding(innerPadding)
+            )
+            return@Scaffold
         }
-    ) {
-        innerPadding ->
         Text(
             text = "Home Page",
             modifier = modifier.padding(innerPadding)
+        )
+    }
+}
+
+@Composable
+fun EmptyList(
+    message: String,
+    modifier: Modifier = Modifier
+) {
+    Surface(
+        modifier = modifier.fillMaxSize().wrapContentSize()
+    ) {
+        Text(
+            message, textAlign = TextAlign.Center
         )
     }
 }
