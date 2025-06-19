03afa92fb3220d608417ffba8775bac4379eabdb
João Pedro <joao.pedro@joinin.com.br>
2025-06-19 11:05:29 -0300

    feat: add shopping events list display

--- app/src/main/java/com/example/shopping_events_app/ui/home/HomePage.kt
+++ app/src/main/java/com/example/shopping_events_app/ui/home/HomePage.kt
@@ -3,6 +3,8 @@ package com.example.shopping_events_app.ui.home
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.FabPosition
@@ -18,6 +20,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.shopping_events_app.customcomp.ShoppingAppBar
+import com.example.shopping_events_app.data.entities.ShoppingEvent
+
 @Composable
 fun HomePage(
     navigateToAddEvent: () -> Unit,
@@ -50,10 +54,24 @@ fun HomePage(
             )
             return@Scaffold
         }
-        Text(
-            text = "Home Page",
+        ShoppingList(
+            uiState.events,
             modifier = modifier.padding(innerPadding)
         )
+    }
+}
+
+@Composable
+fun ShoppingList(
+    shoppingEvents: List<ShoppingEvent>,
+    modifier: Modifier = Modifier
+) {
+    LazyColumn(
+        modifier = modifier
+    ) {
+        items(shoppingEvents) { event ->
+            Text(event.name)
+        }
     }
 }
 
@@ -63,7 +81,9 @@ fun EmptyList(
     modifier: Modifier = Modifier
 ) {
     Surface(
-        modifier = modifier.fillMaxSize().wrapContentSize()
+        modifier = modifier
+            .fillMaxSize()
+            .wrapContentSize()
     ) {
         Text(
             message, textAlign = TextAlign.Center
