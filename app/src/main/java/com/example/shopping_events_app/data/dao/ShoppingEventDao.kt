270a7c2f973bec5a3f6cde2025e588cdb8b278f5
joaosehn2406 <jpssantosos5874@gmail.com>
2025-06-23 09:41:45 -0300

    feat: created SQL to show the events with correspondent item and total cost

--- app/src/main/java/com/example/shopping_events_app/data/dao/ShoppingEventDao.kt
+++ app/src/main/java/com/example/shopping_events_app/data/dao/ShoppingEventDao.kt
@@ -6,6 +6,7 @@ import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
 import com.example.shopping_events_app.data.entities.ShoppingEvent
+import com.example.shopping_events_app.data.entities.ShoppingItem
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -21,4 +22,25 @@ interface ShoppingEventDao {
 
     @Query("SELECT * FROM shopping_events")
     fun getEvents(): Flow<List<ShoppingEvent>>
+
+    @Query("""
+    SELECT 
+        se.id, 
+        se.name, 
+        se.initial_budget, 
+        se.event_date, 
+        se.completed,
+        (SELECT SUM(i2.price * i2.quantity) FROM shopping_items i2 WHERE i2.event_id = se.id) AS total_cost,
+        i.itemId, 
+        i.event_id, 
+        i.item_name, 
+        i.price, 
+        i.quantity
+    FROM shopping_events se
+    LEFT JOIN shopping_items i ON se.id = i.event_id
+    WHERE se.id = :id
+""")
+    suspend fun getEventWithItemsAndTotalCost(id: Long): List<Map<ShoppingEvent, List<ShoppingItem>>>
+
+
 }
